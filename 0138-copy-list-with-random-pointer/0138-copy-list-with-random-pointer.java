3018ddddb4a6298f5c8ebedeb051424fd1809d32
Archithgandla <79374523+Archithgandla@users.noreply.github.com>
2023-01-10 00:14:18 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (59.90%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -13,6 +13,23 @@ class Node {
 }
 */
 
+
+//Approach - 1
+//Time : O(n)
+//Space : O(1)
+//Watch striver : pending
+
+
+
+
+
+
+
+
+
+//Approach - 2
+//Time : O(n)
+//Space : O(n)
 class Solution {
     public Node copyRandomList(Node head) {
         HashMap<Node,Node> hm = new HashMap<>();
@@ -44,3 +61,72 @@ class Solution {
         
     }
 }
+
+
+//Approach - 3
+// class Solution {
+//     public Node copyRandomList(Node head) {
+//         HashMap<Node,Integer> hm = new HashMap<>();
+
+//         Node temp = head;
+
+//         int index = 0;
+//         while(temp != null){
+//             hm.put(temp,index);
+//             index+=1;
+//             temp = temp.next;
+//         }
+//         System.out.println(hm);
+
+//         int[] randoms = new int[hm.size()];
+//         temp = head;
+//         index = 0;
+
+//         while(temp != null){
+
+//             if(temp.random == null){
+//                 randoms[index] = Integer.MAX_VALUE;
+//                 index+=1;
+//                 temp = temp.next;
+//                 continue;
+//             }
+
+
+//             randoms[index] = hm.get(temp.random);
+//             index+=1;
+//             temp = temp.next;
+//         }
+
+//         Node[] arr = new Node[randoms.length];
+//         Node dummy = new Node(-1);
+//         Node t = dummy;
+//         temp = head;
+//         index = 0;
+
+//         while(temp != null){
+//             t.next = new Node(temp.val);
+//             t = t.next;
+//             temp = temp.next;
+
+//             arr[index] = t;
+//             index+=1;
+//         }
+
+//         t = dummy.next;
+//         index = 0;
+
+//         while(t != null){
+
+//             if(randoms[index] == Integer.MAX_VALUE){
+//                 index+=1;
+//                 t = t.next;
+//                 continue;
+//             }
+//             t.random = arr[randoms[index]];
+//             t = t.next;
+//             index+=1;
+//         }
+//         return dummy.next;
+//     }
+// }
+
