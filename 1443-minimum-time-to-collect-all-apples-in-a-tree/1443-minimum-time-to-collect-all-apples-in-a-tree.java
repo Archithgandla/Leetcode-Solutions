522594ac40b5d8b106552bc9cf817e4a9327ff4d
Archithgandla <79374523+Archithgandla@users.noreply.github.com>
2023-01-11 19:33:07 +0530

    Time: 27 ms (95.29%), Space: 85.3 MB (71.18%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.java
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.java
@@ -1,22 +1,65 @@
 class Solution {
 
-    int count;
-    boolean Util(ArrayList<ArrayList<Integer>> adj,List<Boolean> hasApple,int v,boolean[] visited){
+//Since it is mentioned as a tree we donot have cycle and it is enough to take parent 
+//instead of entire parent array
 
-        boolean flag = false;
-        if(hasApple.get(v))
-            flag = true;
-        visited[v] = true;
+//     int count;
+//     boolean Util(ArrayList<ArrayList<Integer>> adj,List<Boolean> hasApple,int v,int parent){
 
+//         boolean flag = false;
+//         if(hasApple.get(v))
+//             flag = true;
+
+//         for(int i:adj.get(v)){
+//             if(i != parent && Util(adj,hasApple,i,v)){
+//                 flag = true;
+//                 count+=2;
+//             }
+//         }
+//         return flag;
+//     }
+
+//     public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
+//         //Constructing a adjacency matrix
+//         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+//         for(int i=0;i<n;i++)
+//             adj.add(new ArrayList<>());
+
+//         for(int[] edge:edges){
+//             adj.get(edge[0]).add(edge[1]);
+//             adj.get(edge[1]).add(edge[0]);
+//         }
+//        // System.out.println(adj);
+
+//         count = 0;
+//         Util(adj,hasApple,0,-1);
+
+//         return count;
+//     }
+    
+    
+    //Better Approach
+    //Without using global variables
+    //Referecne : https://www.youtube.com/watch?v=qSBvKlUq0xo
+    
+    int Util(ArrayList<ArrayList<Integer>> adj,int v,int parent,List<Boolean> hasApple){
+        int count = 0;
+        
         for(int i:adj.get(v)){
-            if(visited[i] != true && Util(adj,hasApple,i,visited)){
-                flag = true;
+            if(i == parent)
+                continue;
+            
+            int from_child = Util(adj,i,v,hasApple);
+            
+            if(hasApple.get(i) == true || from_child>0){
+                count+=from_child;
                 count+=2;
             }
+                
         }
-        return flag;
+        return count;
     }
-
+    
     public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
         //Constructing a adjacency matrix
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
@@ -27,13 +70,10 @@ class Solution {
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
         }
-       // System.out.println(adj);
-        boolean[] visited = new boolean[n];
-
-        count = 0;
-        visited[0] = true;
-        Util(adj,hasApple,0,visited);
-
-        return count;
+        
+        return Util(adj,0,-1,hasApple);
+        
     }
+        
+
 }
