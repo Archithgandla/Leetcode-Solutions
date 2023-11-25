8f032af4ce7412f82348b63b04e9a1f29b3267c6
Archithgandla <79374523+Archithgandla@users.noreply.github.com>
2023-11-25 21:02:40 +0530

    Time: 5 ms (25.64%), Space: 58.1 MB (59.62%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
@@ -2,12 +2,31 @@ class Solution {
     public int[] getSumAbsoluteDifferences(int[] nums) {
         
         
+        //Approach-1
+        //Use a nested loop and for each outer iteration calculate the difference of current element
+        //with of all other elements using inner loop
+        
+        
+        //Approach-2
+        //Since the array is an non decreasing array
+        //Instead of calculating difference seperately
+        //We can just calculate the sum of elements to the left and right of current element
+        //and calculate difference directly
+    
+        //This comes from the property
+        //|a| is a if a is positive
+        //|a| is -a if a is positive
+        
+        //|a| + |b| can be written as a+b if 
+        //a<0 and b<0 or
+        //a>0 and b>0
+        
+        
         int[] pre = new int[nums.length];
         int[] ans = new int[nums.length];
         pre[0] = 0;        
         int sum = 0;
         
-        //5
         
         for(int i=1;i<nums.length;i++){
             pre[i] = pre[i-1]+nums[i-1];
@@ -15,12 +34,9 @@ class Solution {
         }
         sum+=nums[0];
         
-        for(int i=0;i<nums.length;i++){
-            // System.out.println(sum-pre[i]-nums[i]);
-            // System.out.println((nums.length-i-1)*nums[i]);
+        for(int i=0;i<nums.length;i++)
             ans[i] = (i*nums[i]-pre[i])+(sum-pre[i]-nums[i]-((nums.length-i-1)*nums[i]));
-            //System.out.println(" ");
-        }
+        
         return ans;
         
     }
