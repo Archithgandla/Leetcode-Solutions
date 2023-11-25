2e34206e5a86754072b403994ec1dce3acf8d132
Archithgandla <79374523+Archithgandla@users.noreply.github.com>
2023-11-25 21:06:13 +0530

    Time: 3 ms (99.36%), Space: 56.7 MB (89.74%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
@@ -21,23 +21,43 @@ class Solution {
         //a<0 and b<0 or
         //a>0 and b>0
         
+//       Code with prefix array
         
-        int[] pre = new int[nums.length];
-        int[] ans = new int[nums.length];
-        pre[0] = 0;        
-        int sum = 0;
+//         int[] pre = new int[nums.length];
+//         int[] ans = new int[nums.length];
+//         pre[0] = 0;        
+//         int sum = 0;
         
         
-        for(int i=1;i<nums.length;i++){
-            pre[i] = pre[i-1]+nums[i-1];
-            sum+=nums[i];
-        }
-        sum+=nums[0];
+//         for(int i=1;i<nums.length;i++){
+//             pre[i] = pre[i-1]+nums[i-1];
+//             sum+=nums[i];
+//         }
+//         sum+=nums[0];
+        
+//         for(int i=0;i<nums.length;i++)
+//             ans[i] = (i*nums[i]-pre[i])+(sum-pre[i]-nums[i]-((nums.length-i-1)*nums[i]));
+        
+//         return ans;
+        
+        
+        
+        //Without using prefix array
+        int[] ans = new int[nums.length];       
+        int sum = 0,pre = 0;
+        
         
         for(int i=0;i<nums.length;i++)
-            ans[i] = (i*nums[i]-pre[i])+(sum-pre[i]-nums[i]-((nums.length-i-1)*nums[i]));
+            sum+=nums[i];
         
-        return ans;
+        
+        for(int i=0;i<nums.length;i++){
+            ans[i] = (i*nums[i]-pre)+(sum-pre-nums[i]-((nums.length-i-1)*nums[i]));
+            pre+=nums[i];
+        }
+        return ans;   
+        
+        
         
     }
 }
