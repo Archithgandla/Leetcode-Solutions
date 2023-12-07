819846a44c9668d68aa2fd835e5eb2b8ee4786b3
Archithgandla <79374523+Archithgandla@users.noreply.github.com>
2023-12-07 11:29:42 +0530

    Time: 1 ms (100.00%), Space: 44.7 MB (25.45%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
@@ -3,10 +3,12 @@ class Solution {
 
         int idx = -1;        
 
-        for(int i=0;i<num.length();i++){
+        for(int i=num.length()-1;i>=0;i--){
             char c = num.charAt(i);
-            if((int)(c-'0')%2 != 0)
+            if((int)(c-'0')%2 != 0){
                 idx = i;
+                break;
+            }
         }
 
         return idx == -1?"":num.substring(0,idx+1);
