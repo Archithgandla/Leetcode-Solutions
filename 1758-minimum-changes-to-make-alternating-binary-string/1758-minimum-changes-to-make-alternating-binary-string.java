c8a74f75ddc47b2ffdc0ac84e8b332c5c2f81c76
Archithgandla <79374523+Archithgandla@users.noreply.github.com>
2023-12-24 13:00:33 +0530

    Time: 7 ms (9.51%), Space: 42.4 MB (6.56%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -1,6 +1,24 @@
 class Solution {
     public int minOperations(String s) {
         
+        //Approach-1
+        //Recursion approach
+        //For each character there are two possibilities
+        //Flip or non flip
+        //start from index0 and till you reach index s.length()
+        //Now check wheter alternate string is formed
+        //If yes compare it with global minimum variable
+        
+        
+        
+        //Approach-2
+        //We need to make the alternate characters different
+        //For this two patterns exists based on the first character
+        //i.e starting with 0 -> 0101010101.....
+        //or else starting with 1 -> 10101010101.....
+        //Consider both the possibilities and take the minimum
+        
+        
         char prev = s.charAt(0);
         int count1 = 0,count2 = 0;
         
