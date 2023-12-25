4f82061c44f018876bfcbb5fd0e5c0cfadac2499
Archithgandla <79374523+Archithgandla@users.noreply.github.com>
2023-12-25 16:16:20 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (8.23%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -30,10 +30,29 @@ class Solution {
     
     public int numDecodings(String s) {
         
-        int[] dp = new int[s.length()];
-        Arrays.fill(dp,-1);
+        int[] dp = new int[s.length()+1];
         
-        return Util(s,0,dp);
+        dp[s.length()] = 1;
+        
+        for(int i=s.length()-1;i>=0;i--){
+            
+            if(s.charAt(i) == '0'){
+                dp[i] = 0;
+                continue;
+            }
+            
+            
+            int ans = 0;
+        
+            ans+=dp[i+1];
+        
+            if(i != s.length()-1 && (s.charAt(i)-'0')*10+(s.charAt(i+1)-'0')<=26)
+                ans+=dp[i+2];
+        
+            dp[i] = ans;
+        }
+        
+        return dp[0];
                 
     }
 }
