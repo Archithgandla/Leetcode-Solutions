502ee3f7be9fc5e2dae7b6cd18540e65a3b24ccf
Archithgandla <79374523+Archithgandla@users.noreply.github.com>
2024-02-23 21:30:10 +0530

    Time: 6 ms (86.77%), Space: 44.6 MB (34.16%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,21 +1,81 @@
+
+class Pair{
+    
+    int dst,cost;
+    
+    
+    Pair(int dst,int cost){
+        this.dst = dst;
+        this.cost = cost;
+    }
+    
+}
+
+
+
+
 class Solution {
-    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[][] dp = new int[k+2][n];
-        for(int i=0; i<=k+1; i++){
-            Arrays.fill(dp[i],Integer.MAX_VALUE);    
-        }
-        //fly from src to scr cost 0
-        for(int i=0; i<=k+1; i++){
-            dp[i][src] = 0;    
+    
+    
+    
+    public int Util(ArrayList<ArrayList<Pair>> adj,int src,int target,int k,int[][] dp){
+        
+        
+        //System.out.println(src);
+        
+        
+        
+        if(src == target)
+            return 0;
+        
+        else if(k == 0)
+            return Integer.MAX_VALUE;
+        
+        
+        if(dp[src][k] != -1)
+            return dp[src][k];
+            
+        int minm = Integer.MAX_VALUE;
+        
+        for(Pair p:adj.get(src)){
+            
+            int temp = Util(adj,p.dst,target,k-1,dp);
+            
+            if(temp != Integer.MAX_VALUE)
+                minm = Math.min(minm,p.cost+temp);
         }
         
-        for(int i=1; i<=k+1; i++){
-            for(int[] f: flights){
-                if(dp[i-1][f[0]]!=Integer.MAX_VALUE){
-                    dp[i][f[1]] = Math.min(dp[i][f[1]],dp[i-1][f[0]]+f[2]);
-                }
-            }
-        }
-        return dp[k+1][dst] == Integer.MAX_VALUE ? -1 : dp[k+1][dst];
+        return dp[src][k] = minm;
+        
+    }
+    
+
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+                
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+    
+        for(int i=0;i<n;i++)
+            adj.add(new ArrayList<Pair>());
+           
+        for(int[] flight:flights)
+            adj.get(flight[0]).add(new Pair(flight[1],flight[2]));
+        
+        
+        int[][] dp = new int[n+1][k+2];
+        
+        for(int[] arr:dp)
+            Arrays.fill(arr,-1);
+        
+        int ans = Util(adj,src,dst,k+1,dp);
+        
+        
+        return ans == Integer.MAX_VALUE?-1:ans;
+                            
     }
 }
+
+
+
+
+
+
