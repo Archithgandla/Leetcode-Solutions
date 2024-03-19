036978b14f08ec606b414047cb8e4a1ff22b82c1
Archithgandla <79374523+Archithgandla@users.noreply.github.com>
2024-03-19 22:25:37 +0530

    Time: 535 ms (5.10%), Space: 45.2 MB (88.48%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,51 +1,63 @@
+
 class Pair{
+        
     int freq;
-    int time;
-
-    Pair(int freq,int time){
-        this.time = time;
+    int avail_time;
+        
+    Pair(int freq,int avail_time){
         this.freq = freq;
+        this.avail_time = avail_time;
     }
+        
 }
-
 
 
 class Solution {
+    
     public int leastInterval(char[] tasks, int n) {
         
-        int[] freq = new int[26];
-
-        for(int i=0;i<tasks.length;i++)
-            freq[tasks[i]-'A']+=1;
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        Queue<Pair> q = new LinkedList<>();
-
-        int counter = 0;
-
-        for(int i:freq){
-            if(i!=0)
-                pq.add(i);
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> b.freq-a.freq);
+        HashMap<Character,Integer> hm = new HashMap<>();
+        
+        for(char c:tasks)
+            hm.put(c,hm.getOrDefault(c,0)+1);
+              
+        for(char c:hm.keySet())
+            pq.add(new Pair(hm.get(c),0));
+            
+        int time = 0;
+        
+        while(pq.size()>0){
+            
+            
+            ArrayList<Pair> Al = new ArrayList<>();
+            
+            while(pq.size()>0){
+                
+                if(pq.peek().avail_time<=time){
+                    Pair p = pq.poll();
+                    
+                    if(p.freq>1)
+                        pq.add(new Pair(p.freq-1,time+n+1));
+                    break;
+                }
+                Al.add(pq.poll());
+            }
+            
+            for(Pair p:Al)
+                pq.add(p);
+            
+            time+=1;
         }
         
-        while(pq.size()>0 || q.size()>0){
-            if(q.size()>0 && q.peek().time == counter)
-                pq.add(q.poll().freq);
+        return time;
             
-            if(pq.size() == 0){
-                counter+=1;
-                continue;
-            }
-
-
-            int temp = pq.poll();
-            counter+=1;
-
-            if(temp-1>0)
-                q.add(new Pair(temp-1,counter+n));
-
-        }
-        return counter;
-
     }
 }
+
+
+
+
+
+
+
