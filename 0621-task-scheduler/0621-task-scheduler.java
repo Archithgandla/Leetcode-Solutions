20f76b854cf4cefebd9eb05af818c2054113c627
Archithgandla <79374523+Archithgandla@users.noreply.github.com>
2024-03-19 22:39:59 +0530

    Time: 28 ms (36.30%), Space: 45.4 MB (83.40%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -18,6 +18,8 @@ class Solution {
         
         PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> b.freq-a.freq);
         HashMap<Character,Integer> hm = new HashMap<>();
+        Queue<Pair> q = new LinkedList<>();
+        
         
         for(char c:tasks)
             hm.put(c,hm.getOrDefault(c,0)+1);
@@ -27,27 +29,20 @@ class Solution {
             
         int time = 0;
         
-        while(pq.size()>0){
+        while(pq.size()>0 || q.size()>0){
             
+            if(q.size()>0 && q.peek().avail_time<=time)
+                pq.add(q.poll());
             
-            ArrayList<Pair> Al = new ArrayList<>();
-            
-            while(pq.size()>0){
+            if(pq.size()>0){
                 
-                if(pq.peek().avail_time<=time){
-                    Pair p = pq.poll();
-                    
-                    if(p.freq>1)
-                        pq.add(new Pair(p.freq-1,time+n+1));
-                    break;
-                }
-                Al.add(pq.poll());
+                Pair p = pq.poll();
+                
+                if(p.freq>1)
+                    q.add(new Pair(p.freq-1,time+n+1));
             }
-            
-            for(Pair p:Al)
-                pq.add(p);
-            
             time+=1;
+            
         }
         
         return time;
