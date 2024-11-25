c26b7603c15e36c9fbd82e23409243fcf9660d74
Archithgandla <79374523+Archithgandla@users.noreply.github.com>
2024-11-26 01:10:02 +0530

    Time: 49 ms (30.20%), Space: 87 MB (72.17%) - LeetHub

---
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] arrayChange(int[] nums, int[][] operations) {
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++)
+            hm.put(nums[i],i);
+
+        for(int[] arr:operations){
+            nums[hm.get(arr[0])] = arr[1];
+            hm.put(arr[1],hm.get(arr[0]));
+            hm.remove(arr[0]);
+        }
+        return nums;
+    }
+}
+
+
+
+
+
+
+
+
+
+
